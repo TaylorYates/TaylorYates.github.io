5ee18f876d70ee2fd1fd97c0ecda6919af07cd26
Taylor Yates <tayloryates@Taylor-Yates.local>
2018-07-09 11:34:00 +1000

    Design updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
+function startUp() {
+  $("span").css("display", "none");
+  $(".about").css("display", "block");
+  $(".aboutLink").css("font-style", "italic");
+
+}
+
 function hideSection() {
  $("span").fadeOut(200);
 }
 
 function about() {
-  $(".background").css("background-color", "#FF6666");
   $(".about").fadeIn(500);
 }
 
 function cv() {
-  $(".background").css("background-color", "#CCFF66");
   $(".cv").fadeIn(500);
 }
 
 function portfolio() {
-  $(".background").css("background-color", "#31E3C3");
   $(".portfolio").fadeIn(500);
 }
 
 function contact() {
-  $(".background").css("background-color", "#BD7ECF");
   $(".contact").fadeIn(500);
 }
 
 function home() {
-  $(".background").css("background-color", "#FFD454");
   $("nav a").css("font-style", "normal");
 }
 
@@ -33,6 +35,7 @@ function activeLink() {
 }
 
 function start() {
+  startUp();
   $(".hideSpans").click(hideSection);
   $(".aboutLink").click(about);
   $(".portfolioLink").click(portfolio);
