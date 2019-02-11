2384f173551dc441a233575b20f4957c92e5907d
Taylor Yates <taylor.yates@live.com>
2019-02-11 17:20:55 +1100

    Minor updates

--- gulpfile.js
+++ gulpfile.js
@@ -9,5 +9,5 @@ gulp.task('styles', function() {
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('scss/**/*.scss',['styles']);
+    gulp.watch('scss/**/*.scss', gulp.series('styles'));
 });
